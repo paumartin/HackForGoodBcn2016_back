32711991b7f32bdc069074717ae4f4ee50b09cf3
Pau Martin <kingdoncom@gmail.com>
2016-02-27 01:21:12 +0100

    asdasd

--- backend/pis.router.js
+++ backend/pis.router.js
@@ -58,7 +58,7 @@ router.post('/update', function(req, res) {
 router.get('/get/:pisId', function(req, res) {
   var collection = req.db.collection(collectionName);
   var id = mongo.ObjectID(req.params.pisId);
-  collection.find({ _id : id }, function(error, data) {
+  collection.find({_id : id}).toArray(function(error, data) {
     if (error) res.status(500).json(error);
     else res.status(200).json(data);
   });
